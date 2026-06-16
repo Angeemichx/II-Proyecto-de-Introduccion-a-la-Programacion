7a359698a67667fb26101b2087fff5242e0ac5db
Angeemichx <michelle12angelica@gmail.com>
2026-06-16 10:03:30 -0600

    Se crea el movimiento de un personaje con las teclas W-A-S-D

--- interfaz.py
+++ interfaz.py
@@ -1,21 +1,77 @@
-<<<<<<< HEAD
 import pygame
+
+#Se utiliza init para que las funciones dentro de pygame puedan funcionar 
 pygame.init()
 
-ANCHO = 800
-ALTO = 600
+#Se desarrollan ciertas variables que se utilizaran constantemente con el fin de facilitar su modificación de ser necesario
+ANCHO = 1280
+ALTO = 720
 ROJO = (255, 0, 0)
+NEGRO = (0,0,0)
+VERDE = (0, 255, 0)
+personaje_pos_x = 10
+personaje_pos_y = 10
+personaje_velocidad_x = 0
+personaje_velocidad_y = 0
 
-ventana = pygame.display.set_mode((ANCHO, ALTO)) 
+#Con esto, se crea la ventana principal 
+ventana = pygame.display.set_mode((ANCHO, ALTO), pygame.FULLSCREEN) 
+reloj = pygame.time.Clock()
 
-pygame.time.delay(4000)
-
+#Esta función permite que la pantalla no se cierre mientras se está jugando, además se añade la x superior derecha y la tecla esc para cerrar la ventana (salir del bucle)
 jugando = True 
-while jugando:
-    ventana.fill(ROJO)
-    pygame.display.update
 
+while jugando:
+
+    reloj.tick(60)
+
+    for event in pygame.event.get():
+        print(event)
+        if event.type == pygame.QUIT:
+            jugando = False 
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                jugando = False 
+
+            #Controles cuando se presiona una tecla
+            if event.key == pygame.K_d:
+                personaje_velocidad_x = 10
+
+            if event.key == pygame.K_a:
+                personaje_velocidad_x = -10
+
+            if event.key == pygame.K_s:
+                personaje_velocidad_y = 10
+
+            if event.key == pygame.K_w:
+                personaje_velocidad_y = -10
+        
+        #Controles cuando se suelta una tecla 
+        if event.type == pygame.KEYUP:
+        
+            if event.key == pygame.K_d:
+                personaje_velocidad_x = 0
+
+            if event.key == pygame.K_a:
+                personaje_velocidad_x = 0
+
+            if event.key == pygame.K_s:
+                personaje_velocidad_y = 0
+
+            if event.key == pygame.K_w:
+                personaje_velocidad_y = 0
+
+#Lógica 
+    personaje_pos_x += personaje_velocidad_x
+    personaje_pos_y += personaje_velocidad_y
+
+#Se configurarán los dibujos de la pantalla
+    ventana.fill(ROJO)
+    pygame.draw.rect(ventana, VERDE, (personaje_pos_x, personaje_pos_y ,100,100))
+
+#Actualizar (se produce lo guardado en memoria)
+    pygame.display.update()
+
+#Permitir que el juego tenga un final y no se ejecuten las funciones para siempre en el bucle
 pygame.quit()
-=======
-#prueba commit
->>>>>>> ef70592dac42094a8109c3614c9e55de0bbc328c
